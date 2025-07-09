f5b3bdaa4a339981af49aa814190bf5dac1e12d6
chan-max <jackieontheway666@gmail.com>
2025-07-10 07:53:36 +0800

    dev

--- src/layout/components/ToolHeader.vue
+++ src/layout/components/ToolHeader.vue
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-04-01 07:04:47
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-07-10 05:36:21
+ * @LastEditTime: 2025-07-10 07:36:15
  * @FilePath: /yishe-admin/src/layout/components/ToolHeader.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -89,7 +89,7 @@ function openDesignTool() {
   const baseUrl = import.meta.env.PROD ? 'http://49.232.186.238:1522' : 'http://localhost:1522'
   const url = `${baseUrl}/#/design?token=${encodeURIComponent(token)}`
   messenger = new NativeWindowMessenger()
-  messenger.openChild(url, '_blank')
+  messenger.openChild(url)
   // 监听子窗口消息
   messenger.on('customEvent', (data) => {
     ElMessage.success('收到子窗口消息: ' + JSON.stringify(data))
--- src/utils/nativeWindowMessenger.ts
+++ src/utils/nativeWindowMessenger.ts
@@ -5,12 +5,42 @@ export class NativeWindowMessenger {
   private handlers: Record<string, MessageHandler[]> = {}
 
   openChild(url: string, target = '_blank') {
-    this.childWindow = window.open(url, target)
-    window.addEventListener('message', this._onMessage)
+    // 定义独立窗口的特性参数
+    const windowFeatures = [
+      'width=1200',
+      'height=800',
+      'left=100',
+      'top=100',
+      'resizable=yes',
+      'scrollbars=yes',
+      'status=yes',
+      'toolbar=no',
+      'menubar=no',
+      'location=no',
+      'directories=no',
+      'copyhistory=no'
+    ].join(',')
+
+    this.childWindow = window.open(url, target, windowFeatures)
+    
+    // 检查窗口是否成功打开
+    if (this.childWindow) {
+      window.addEventListener('message', this._onMessage)
+      
+      // 监听子窗口关闭事件
+      const checkClosed = setInterval(() => {
+        if (this.childWindow?.closed) {
+          clearInterval(checkClosed)
+          this.destroy()
+        }
+      }, 1000)
+    } else {
+      console.error('无法打开独立窗口，可能是被浏览器阻止了弹窗')
+    }
   }
 
   send(type: string, data: any) {
-    if (this.childWindow) {
+    if (this.childWindow && !this.childWindow.closed) {
       this.childWindow.postMessage({ type, data }, '*')
     }
   }
@@ -29,6 +59,9 @@ export class NativeWindowMessenger {
 
   destroy() {
     window.removeEventListener('message', this._onMessage)
+    if (this.childWindow && !this.childWindow.closed) {
+      this.childWindow.close()
+    }
     this.childWindow = null
     this.handlers = {}
   }
