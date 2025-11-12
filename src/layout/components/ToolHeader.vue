5f24059f93b2bec1664531a2b0d6fa1ced4e5e88
chan-max <jackieontheway666@gmail.com>
2025-11-13 06:49:20 +0800

    remove tool floating button

--- src/layout/components/ToolHeader.vue
+++ src/layout/components/ToolHeader.vue
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-04-01 07:04:47
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-11-12 19:14:23
+ * @LastEditTime: 2025-11-13 06:22:44
  * @FilePath: /yishe-admin/src/layout/components/ToolHeader.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -145,7 +145,7 @@ export default defineComponent({
     
           <UserInfo></UserInfo>
 
-          <FloatingUtilityButton/>
+          {/* <FloatingUtilityButton/> */}
         </div>
       </div>
     )
