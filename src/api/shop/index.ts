77ee57644b8ce0fc89a1a41e4e806e208779c45a
chan-max <jackieontheway666@gmail.com>
2026-02-14 13:09:08 +0800

    dev

--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -12,25 +12,25 @@ export interface Shop {
 
 // Get Shop List
 export const getShopList = () => {
-  return request.get({ url: '/shop' })
+  return request.get({ url: '/operations/shop' })
 }
 
 // Get Shop Detail
 export const getShopDetail = (id: number) => {
-  return request.get({ url: `/shop/${id}` })
+  return request.get({ url: `/operations/shop/${id}` })
 }
 
 // Create Shop
 export const createShop = (data: Partial<Shop>) => {
-  return request.post({ url: '/shop', data })
+  return request.post({ url: '/operations/shop', data })
 }
 
 // Update Shop
 export const updateShop = (id: number, data: Partial<Shop>) => {
-  return request.patch({ url: `/shop/${id}`, data })
+  return request.patch({ url: `/operations/shop/${id}`, data })
 }
 
 // Delete Shop
 export const deleteShop = (id: number) => {
-  return request.delete({ url: `/shop/${id}` })
+  return request.delete({ url: `/operations/shop/${id}` })
 }
